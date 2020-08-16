1c67a2071418254125c43dc9ff5f0f158b788997
ht <w00110601111@163.com>
2020-08-16 15:27:04 +0800

    fff

--- SpringBootUploadAndDownload/src/main/java/hello/web/QueryController2.java
+++ SpringBootUploadAndDownload/src/main/java/hello/web/QueryController2.java
@@ -2,20 +2,44 @@ package hello.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 
 @RequestMapping(value = "/users")
 public class QueryController2 {
 
-	@RequestMapping(value = { "" }, method = RequestMethod.GET)
-	 @ApiOperation(value="获取所有用户列表", notes="")
-	public List<String> getUserList() {
+	@RequestMapping(value = { "ddd" }, method = RequestMethod.GET)
+
+	public List<String> getUserList(String name, @RequestBody String id) {
+
+		System.out.println(name);
+		System.out.println(id);
+
+		return null;
+
+	}
+
+	@RequestMapping(value = { "ddd2" }, method = RequestMethod.POST)
+
+	public List<String> getUserList2(String name, String id) {
+
+		System.out.println("ddddd=====" + name);
+		System.out.println("id=====" + id);
+
+		return null;
+
+	}
+
+	@RequestMapping(value = { "333" }, method = RequestMethod.POST)
+
+	public List<String> fsfddsf(String name, @RequestBody String id) {
+
+		System.out.println("ddddd=====" + name);
+		System.out.println("id=====" + id);
 
 		return null;
 
